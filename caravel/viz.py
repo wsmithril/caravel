30e57b073644425c80c563566d60244f862f1412
George Ke <georgekeee@gmail.com>
2016-06-13 11:14:54 -0700

    Minor fixes, label field affects points, text changes

--- caravel/viz.py
+++ caravel/viz.py
@@ -1665,10 +1665,6 @@ class MapboxViz(BaseViz):
             'clustering_radius',
             'point_radius',
             'point_radius_unit',
-        )
-    }, {
-        'label': 'Clustering',
-        'fields': (
             'all_columns',
             'pandas_aggfunc',
         )
@@ -1689,7 +1685,7 @@ class MapboxViz(BaseViz):
             'description': "Column containing latitude data",
         },
         'all_columns': {
-            'label': 'Cluster label',
+            'label': 'Label',
             'description': (
                 "Numerical columns will be aggregated with the aggregator. "
                 "Non-numerical columns will be used to label points."
